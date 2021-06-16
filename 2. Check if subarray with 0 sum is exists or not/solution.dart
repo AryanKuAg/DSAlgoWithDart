9b13d2c746e983dcc269773ab12ac84c77138506
AryanKuAg <alemantrixaryan@gmail.com>
2021-06-16 16:41:22 +0545

    new one added

---
+++ 2. Check if subarray with 0 sum is exists or not/solution.dart
@@ -0,0 +1,18 @@
+/*
+Input:  { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+ 
+Output: Subarray with zero-sum exists
+ 
+The subarrays with a sum of 0 are:
+ 
+{ 3, 4, -7 }
+{ 4, -7, 3 }
+{ -7, 3, 1, 3 }
+{ 3, 1, -4 }
+{ 3, 1, 3, 1, -4, -2, -2 }
+{ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+*/
+
+void main() {
+  
+}
